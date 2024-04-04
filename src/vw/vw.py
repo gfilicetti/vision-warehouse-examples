1772164a07a9242065fef0addac02981f8151690
Gino Filicetti <ginof@google.com>
2024-04-04 13:41:52 -0400

    Finally figured out the project client... on our way to copying everything.

--- src/vw/vw.py
+++ src/vw/vw.py
@@ -13,22 +13,16 @@ from google.cloud import resourcemanager_v3 as resourcemanager
 
 def get_project_number():
     rm = resourcemanager.ProjectsClient()
-    project = rm.get_project(name="projects/55194013451")
-    # request = resourcemanager.GetProjectRequest(name="projects/vision-warehouse-pilot")
-    # project = projects_client.get_project(request=request)
+    project = rm.get_project(name="projects/vision-warehouse-pilot")
 
-    print(project.name)
-    print(project.display_name)
-    print(project.project_id)
-
-    return project
+    # name comes back as "projects/5519466666", we only want to return the number
+    return project.name.split('/')[1]
 
 def main(args):
     yay = "Yes!"
     print(f"You made it inside. {yay}")
 
     PROJECT_NUMBER_STR = get_project_number()
-    exit()
     PROJECT_NUMBER = int(PROJECT_NUMBER_STR[0])
 
     # Only us-central1 is supported.
@@ -48,12 +42,23 @@ def main(args):
     CLEAN_UP_INDEX = args.clean_index
     CLEAN_UP_CORPUS = args.clean_corpus
 
+    # Because it takes ~1h to create and deploy index. A existing index can be specified to save time.
+
+    # If CORPUS_ID is specified, skip creating a new corpus.
+    CORPUS_ID = args.corpus_id
+    # If DEPLOYED_INDEX_ID is specified, use existing index instead of creating and deploying a new index.
+    DEPLOYED_INDEX_ID = args.index_id 
+
+    
+
 if __name__ == "__main__":
     parser = ArgumentParser(description="Vision Warehouse Asset Ingest")
     parser.add_argument("--corpus", type=str, help="The name of the Corpus to create", default=os.getenv("VW_CORPUS_NAME", "my_vw_corpus_name"))
     parser.add_argument("--corpus-desc", type=str, help="A description of this Corpus", default=os.getenv("VW_CORPUS_DESC", "This is my VW Corpus"))
     parser.add_argument("--index", type=str, help="The name of the Index to create", default=os.getenv("VW_INDEX_NAME", "my_vw_index"))
     parser.add_argument("--index-endpoint", type=str, help="The name of the Index Endpoint to create", default=os.getenv("VW_INDEX_ENDPOINT_NAME", "my_vw_endpoint"))
+    parser.add_argument("--corpus-id", type=str, help="The id of an existing Corpus, if given we skip creation", default=None)
+    parser.add_argument("--index-id", type=str, help="The id of an existing Index, if given we skip creation", default=None)
     parser.add_argument("--clean-corpus", help="Clean up Corpus", action=BooleanOptionalAction)
     parser.add_argument("--clean-assets", help="Clean up Assets", action=BooleanOptionalAction)
     parser.add_argument("--clean-index", help="Clean up Index", action=BooleanOptionalAction)
